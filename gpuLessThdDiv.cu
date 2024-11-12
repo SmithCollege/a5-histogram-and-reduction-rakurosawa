1be2d29bc08f07073e10fb0dfcbc6fdb433a6466
Ronin Akurosawa <rakurosawa@smith.edu>
2024-11-12 17:51:44 -0500

    less thread division multiple block implementation

--- gpuLessThdDiv.cu
+++ gpuLessThdDiv.cu
@@ -1,82 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define NUM_THDS 5
-// #define BLOCK_SIZE 1024
+#define NUM_THDS 2048
+#define BLOCK_SIZE 1024
 
 __global__ void lessThdDiv(float* arr, char opperation){
 
+    int globalIdx = threadIdx.x + (BLOCK_SIZE * blockIdx.x);
+
     for (int i = NUM_THDS; i > 0; i /= 2){
         __syncthreads();
-        if(threadIdx.x <= i){
-            int idx1 = threadIdx.x * 2;
+        if (globalIdx < i){
+            int idx1 = globalIdx;
             float val1 = arr[idx1];
-            int idx2 = (threadIdx.x * 2) + 1; 
+            int idx2 = idx1 + i;
             float val2 = arr[idx2];
 
             // MAX
             if (opperation == 'x'){
                 if (val1 > val2){
-                    arr[threadIdx.x] = val1;
+                    arr[idx1] = val1;
                 }
                 else {
-                    arr[threadIdx.x] = val2;
+                    arr[idx1] = val2;
                 }
             }
 
             // MIN
-            else if (opperation == 'n'){
+             else if (opperation == 'n'){
                 if (val1 < val2){
-                    arr[threadIdx.x] = val1;
+                    arr[idx1] = val1;
                 }
                 else {
-                    arr[threadIdx.x] = val2;
+                    arr[idx1] = val2;
                 }
             }
 
             // SUM
             else if (opperation == 's'){
-                // adjusting values to prevent calculation errors 
-                arr[idx1] = 0;
-                arr[idx2] = 0;
                 float locSum = val1 + val2;
-                arr[threadIdx.x] = locSum;
+                arr[idx1] = locSum;
             }
 
             // PROUDUCT
             else if (opperation == 'p'){
-                // adjusting values to prevent calculation errors 
-                arr[idx1] = 1;
-                arr[idx2] = 1;
                 float locProd = val1 * val2;
-                arr[threadIdx.x] = locProd;
+                arr[idx1] = locProd;
             }
         }
     }
-    __syncthreads();
-
-    // edge case of the last two values that still need to be dealt with:
-
-    // MAX
-    if (opperation == 'x' && arr[0] < arr[1]){
-        arr[0] = arr[1];
-    }
-
-    // MIN
-    else if (opperation == 'n' && arr[0] > arr[1]){
-        arr[0] = arr[1];
-    }
-
-    // SUM
-    else if (opperation == 's'){
-        arr[0] = arr[0] + arr[1];
-    }
-
-    // PRODUCT
-    else if (opperation == 'p'){
-        arr[0] = arr[0] * arr[1];
-    }
-
     __syncthreads();
 }
 
@@ -85,16 +57,46 @@ int main(){
     float *input;
     cudaMallocManaged(&input, (NUM_THDS*2)*sizeof(float));
 
+    // MAX
     for (int i = 0; i < (NUM_THDS*2); i++) {
-        input[i] = i;
+        input[i] = 1.0;
     }
+    input[0] = 0.0;
 
-    lessThdDiv<<<1, NUM_THDS>>>(input, 'p');
+    lessThdDiv<<<NUM_THDS/BLOCK_SIZE, BLOCK_SIZE>>>(input, 'x');
     cudaDeviceSynchronize();
-    // printf("max value in arr is: %f\n", input[0]);
-    // printf("min value in arr is: %f\n", input[0]);
-    // printf("sum of arr is: %f\n", input[0]);
+    printf("max value in arr is: %f\n", input[0]);
+
+    // MIN
+    for (int i = 0; i < (NUM_THDS*2); i++) {
+        input[i] = 1.0;
+    }
+    input[0] = 0.0;
+
+    lessThdDiv<<<NUM_THDS/BLOCK_SIZE, BLOCK_SIZE>>>(input, 'n');
+    cudaDeviceSynchronize();
+    printf("min value in arr is: %f\n", input[0]);
+
+    // SUM
+    for (int i = 0; i < (NUM_THDS*2); i++) {
+        input[i] = 1.0;
+    }
+    input[0] = 0.0;
+
+    lessThdDiv<<<NUM_THDS/BLOCK_SIZE, BLOCK_SIZE>>>(input, 's');
+    cudaDeviceSynchronize();
+    printf("sum of arr is: %f\n", input[0]);
+
+    // PRODUCT
+    for (int i = 0; i < (NUM_THDS*2); i++) {
+        input[i] = 1.0;
+    }
+    input[0] = 0.0;
+
+    lessThdDiv<<<NUM_THDS/BLOCK_SIZE, BLOCK_SIZE>>>(input, 'p');
+    cudaDeviceSynchronize();
     printf("product arr is: %f\n", input[0]);
+
 
     cudaFree(input);
 }
